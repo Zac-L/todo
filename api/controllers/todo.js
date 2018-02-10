fbc4afc1907415deefa46d46a81e10792f1387bd
Zac-L <Zachlichens@gmail.com>
2018-02-10 14:56:55 -0800

    routes for todo app

---
+++ api/controllers/todo.js
@@ -0,0 +1,92 @@
+const Todo = require('../models/todo');
+
+
+const index = (req, res, next) => {
+  Todo
+    .find({ user: req.user._id })
+    .select('__v -user -createdAt -updateAt')
+    .lean()
+    .then(todos => {
+      res.json({
+        data: {
+          todos,
+        },
+      });
+    })
+    .catch(next);
+};
+
+const store = (req, res, next) => {
+  Todo
+    .create({
+      ...req.body,
+      user: req.user._id
+    })
+    .then(todo => {
+      res.json({
+        data: {
+          todo,
+        },
+      });
+    })
+    .catch(next);
+};
+
+const show = (req, res, next) => {
+  Todo
+    .findOne({
+      _id: req.params._id,
+      user: req.user._id
+    })
+    .select('__v -user -createdAt -updateAt')
+    .lean()
+    .then(todo => {
+      res.json({
+        data: { todo }
+      });
+    })
+    .catch(next);
+};
+
+const update = (req, res, next) => {
+  Todo
+    .findOneAndUpdate({
+      _id: req.params._id,
+      user: req.user._id
+    }, 
+    req.body,
+    { new: true }
+    )
+    .select('__v -user -createdAt -updateAt')
+    .lean()
+    .then(todo => {
+      res.json({
+        data: { todo }
+      });
+    })
+    .catch(next);
+};
+
+const destroy = (req, res, next) => {
+  Todo
+    .findOneAndRemove({
+      _id: req.params._id,
+      user: req.user._id
+    })
+    .select('__v -user -createdAt -updateAt')
+    .lean()
+    .then(todo => {
+      res.json({
+        data: { todo }
+      });
+    })
+    .catch(next);
+};
+
+module.exports = {
+  index,
+  store,
+  show,
+  update,
+  destroy,
+};
