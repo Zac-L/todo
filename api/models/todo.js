150e339700cc936da88e96e71aa73f3f5eb2e3b0
Zac-L <Zachlichens@gmail.com>
2018-02-10 14:56:12 -0800

    setup basic schema

---
+++ api/models/todo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const { Types } = Schema;
+
+const todoSchema = new Schema({
+  title: {
+    type: Types.String,
+    required: [true, 'The title is required.'],
+    trim: true
+  },
+  completed: {
+    type: Types.Boolean,
+    default: false,
+  },
+  user: {
+    type: Types.ObjectId,
+    required: 'The userid is required',
+    ref: 'User',
+  },
+  createdAt: {
+    type: Types.Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Types.Date,
+    default: Date.now,
+  },
+}, {
+  collection: 'todos',
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+module.exports =  Todo;
