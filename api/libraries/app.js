c4707d82884d558e57656ffbd0e4067966fcf018
Zac-L <Zachlichens@gmail.com>
2018-02-17 13:18:05 -0800

    added cors

--- api/libraries/app.js
+++ api/libraries/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('../middlewares/logger');
 const apiRoutes = require('../routes/api');
 
 const app = express();
+
+app.use(cors());
 
 app.use(logger);
 app.use('/v1', apiRoutes);
