e3a3392f8234c32d52160d71e9947a41ef5c1f46
Zac-L <Zachlichens@gmail.com>
2018-02-23 12:36:55 -0800

    updated to include bcrypt / jwt

--- api/constants/env.js
+++ api/constants/env.js
@@ -1,17 +1,21 @@
 const {
-    HOST = '0.0.0.0',
-    NODE_ENV: NODE = 'development',
-    PORT = 8000,
-    PORT_TEST = 8888,
-    MONGODB_URI = 'mongodb://localhost:27017/todo',
-    MONGODB_URI_TEST = 'mongodb://localhost:27017/todo-test'
+  BCRYPT_SALT_ROUNDS = 8,
+  HOST = '0.0.0.0',
+  JWT_SECRET = 'secret',
+  NODE_ENV: NODE = 'development',
+  PORT = 8000,
+  PORT_TEST = 8888,
+  MONGODB_URI = 'mongodb://localhost:27017/todo',
+  MONGODB_URI_TEST = 'mongodb://localhost:27017/todo-test'
 } = process.env;
 
 module.exports = {
-    HOST,
-    NODE,
-    PORT,
-    PORT_TEST,
-    MONGODB_URI,
-    MONGODB_URI_TEST
+  BCRYPT_SALT_ROUNDS,
+  HOST,
+  JWT_SECRET,
+  NODE,
+  PORT,
+  PORT_TEST,
+  MONGODB_URI,
+  MONGODB_URI_TEST
 };
