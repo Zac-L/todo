ebcf544813742c69b068128e63dafcfb7abc9b6c
Zac-L <Zachlichens@gmail.com>
2018-02-27 15:48:00 -0800

    white space

--- app-react/src/data/store.js
+++ app-react/src/data/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
+
 const rootReducer = combineReducers({});
 
 const enhancer = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose)(
