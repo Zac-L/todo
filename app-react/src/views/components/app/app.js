5eeee3fa7f81519a1b223c9ffc30107c972b26db
Zac-L <Zachlichens@gmail.com>
2018-02-28 21:07:00 -0800

    brought in signup/login/todos pages(links)

--- app-react/src/views/components/app/app.js
+++ app-react/src/views/components/app/app.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from 'views/pages/home/home';
+import LoginPage from 'views/pages/login/login';
+import SignupPage from 'views/pages/signup/signup';
+import TodosPage from 'views/pages/todos-list/todos-list';
 import NotFoundPage from 'views/pages/not-found/not-found';
+
 
 
 class App extends Component {
@@ -10,6 +14,9 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={SignupPage} />
+          <Route exact path="/todos" component={TodosPage} />
           <Route component={NotFoundPage} />
         </Switch>
       </Router>
