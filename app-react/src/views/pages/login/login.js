630238815b1cc4785c00cdbd506197b58066c096
Zac-L <Zachlichens@gmail.com>
2018-02-28 21:09:53 -0800

    built out a basic component to handle todos and login, brought in default component to handle linking

---
+++ app-react/src/views/pages/login/login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import DefaultTemplate from 'views/templates/default/default';
+
+
+class Login extends Component {
+
+  render() {
+
+    return(
+      <DefaultTemplate>
+        <h1>Login Page</h1>
+      </DefaultTemplate>
+    );
+  }
+}
+
+export default Login;
---
+++ app-react/src/views/pages/todos-list/todos-list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import DefaultTemplate from 'views/templates/default/default';
+
+
+class TodosPage extends Component {
+
+  render() {
+
+    return(
+      <DefaultTemplate>
+        <h1>TodosPage</h1>
+      </DefaultTemplate>
+    );
+  }
+}
+
+export default TodosPage;
